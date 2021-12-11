c1cf932bc2c0a78febde122ab2d5b2e6fc2ee7c3
Alby <alberto.consorzio@gmail.com>
2021-12-11 15:44:08 +0100

    Test To upload it on git

---
+++ GARC_2_2.py
@@ -0,0 +1,78 @@
+from random import gauss
+import matplotlib.pyplot as plt
+import numpy as np
+from arch import arch_model
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+
+
+# The first step is the creation of a database. 
+
+def p(x):
+    print(x)
+
+
+
+# create dataset
+n = 1000
+omega = 0.5
+
+alpha_1 = 0.1
+alpha_2 = 0.2
+
+beta_1 = 0.3
+beta_2 = 0.4
+
+test_size = int(n*0.1)
+
+series = [gauss(0,1), gauss(0,1)]
+vols = [1, 1]
+
+for _ in range(n):
+    new_vol = np.sqrt(omega + alpha_1*series[-1]**2 + alpha_2*series[-2]**2 + beta_1*vols[-1]**2 + beta_2*vols[-2]**2)
+    new_val = gauss(0,1) * new_vol
+    
+    series.append(new_val)
+    vols.append(new_vol)
+
+plt.figure(figsize=(10,4))
+plt.plot(series)
+plt.title('Simulated GARCH(2,2) Data', fontsize=20)
+plt.show() 
+
+plt.figure(figsize=(10,4))
+plt.plot(vols)
+plt.title('Data Volatility', fontsize=20)
+plt.show() 
+
+plt.figure(figsize=(10,4))
+plt.plot(series)
+plt.plot(vols, color='red')
+plt.title('Data and Volatility', fontsize=20)  
+plt.show() 
+
+# PACF Plot
+plot_pacf(np.array(series)**2)
+plt.show()
+
+""" # Fit the GARCH Model
+train, test = series[:-test_size], series[-test_size:]
+model = arch_model(train, p=2, q=2)
+model_fit = model.fit()
+model_fit.summary() """
+
+
+""" # Predict
+predictions = model_fit.forecast(horizon=test_size)
+plt.figure(figsize=(10,4))
+true, = plt.plot(vols[-test_size:])
+preds, = plt.plot(np.sqrt(predictions.variance.values[-1, :]))
+plt.title('Volatility Prediction', fontsize=20)
+plt.legend(['True Volatility', 'Predicted Volatility'], fontsize=16)
+predictions_long_term = model_fit.forecast(horizon=1000)
+plt.figure(figsize=(10,4))
+true, = plt.plot(vols[-test_size:])
+preds, = plt.plot(np.sqrt(predictions_long_term.variance.values[-1, :]))
+plt.title('Long Term Volatility Prediction', fontsize=20)
+plt.legend(['True Volatility', 'Predicted Volatility'], fontsize=16) """
+ 
+
